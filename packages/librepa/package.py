8f88879fe9a6778d7c0647640d5a2ce565f137b2
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2020-05-08 09:34:31 +0200

    Remove CGAL dependency

--- packages/librepa/package.py
+++ packages/librepa/package.py
@@ -17,7 +17,6 @@ class Librepa(CMakePackage):
     variant('p4est', default=True, description='Enable p4est-based load-balancer')
     variant('kdpart', default=True, description='Enable kdpart-based load-balancer')
     variant('parmetis', default=True, description='Enable ParMETIS-based load-balancer')
-    variant('cgal', default=True, description='Enable CGAL-based load-balancer')
 
     depends_on('cmake@3.12.0:', type='build')
 
@@ -26,7 +25,6 @@ class Librepa(CMakePackage):
     depends_on('boost+serialization+mpi@1.67.0')
     depends_on('p4est-lahnerml', when='+p4est')
     depends_on('kdpart', when='+kdpart')
-    depends_on('cgal', when='+cgal')
     depends_on('parmetis+shared', when='+parmetis')
 
     sanity_check_is_file = ['include/repa/repa.hpp', 'lib/librepa.so']
@@ -37,7 +35,6 @@ class Librepa(CMakePackage):
             "-DWITH_P4EST={}".format("ON" if "+p4est" in spec else "OFF"),
             "-DWITH_KDPART={}".format("ON" if "+kdpart" in spec else "OFF"),
             "-DWITH_PARMETIS={}".format("ON" if "+parmetis" in spec else "OFF"),
-            "-DWITH_CGAL={}".format("ON" if "+cgal" in spec else "OFF"),
             "-DWITH_TESTS=on",
             "-DWITH_COVERAGE=off",
         ]
